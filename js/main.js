450836d297720cca4c8eea059101084f93e4d957
MINECRAFTGOLDFISH3 <152801036+MINECRAFTGOLDFISH3@users.noreply.github.com>
2024-05-04 13:01:45 -0500

    git commit -m

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(";");
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == " ") c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
 
+getCookie('title');
+if (!(title == "")) {
+    document.title = ca;
+}
