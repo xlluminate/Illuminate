b8866e657fe17d2eef9b70f24f1698c1e0f88f2a
MINECRAFTGOLDFISH3 <152801036+MINECRAFTGOLDFISH3@users.noreply.github.com>
2024-06-07 17:33:45 -0500

    Update projects-list.js

--- js/projects-list.js
+++ js/projects-list.js
@@ -97,3 +97,33 @@ document.addEventListener('DOMContentLoaded', () => {
         setupGames();
     });
 });
+
+function search() {
+    document.querySelector('#game-list').innerHTML = '';
+    writeAll();
+    value = document.getElementById('search').value;
+    elements = [];
+    elementsNew = [];
+    console.log(document.querySelector('.game-item').textContent);
+    if (!(value == '')) {
+        for (let i = 0; i < num; i++) {
+            console.log(document.querySelector('.game-item').textContent.toLowerCase().includes(value.toLowerCase()));
+            if (document.querySelector('.game-item').textContent.toLowerCase().includes(value.toLowerCase())) {
+                //document.querySelector('.game-item').style.display = 'block';
+                elementsNew.push('<div class="game-item">' + document.querySelector('.game-item').innerHTML + '</div>');
+                document.querySelector('.game-item').remove();
+            } else {
+                //document.querySelector('.game-item').style.display = 'none';
+                elements.push('<div class="game-item">' + document.querySelector('.game-item').innerHTML + '</div>');
+                document.querySelector('.game-item').remove();
+            }
+        }
+        //document.querySelector('#game-list').innerHTML = elementsNew;
+        document.querySelector('#game-list').innerHTML = '';
+        writeNew();
+    } else {
+        //document.querySelector('#game-list').innerHTML = allElements;
+        document.querySelector('#game-list').innerHTML = '';
+        writeAll();
+    }
+}
