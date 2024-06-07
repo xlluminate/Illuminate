86c072e3255d4ae167587bdb7bc0562c24ffab4c
MINECRAFTGOLDFISH3 <152801036+MINECRAFTGOLDFISH3@users.noreply.github.com>
2024-06-07 14:24:16 -0500

    Update projects.js

--- js/projects.js
+++ js/projects.js
@@ -4,6 +4,7 @@ function initGames() {
     for (let i = 0; i < num; i++) {
         allElements.push('<div class="game-item">' + document.querySelector('.game-item').innerHTML + '</div>');
         document.querySelector('.game-item').remove();
+    }
 }
 //document.querySelector('#game-list').innerHTML = allElements;
 function writeAll() {
