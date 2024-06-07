b3aaef0fd01c9ea96fd6f2c44f5e7d35c0d221d1
MINECRAFTGOLDFISH3 <152801036+MINECRAFTGOLDFISH3@users.noreply.github.com>
2024-06-07 16:52:59 -0500

    Update projects-list.js

--- js/projects-list.js
+++ js/projects-list.js
@@ -63,9 +63,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
             const gameItem = document.createElement('div');
             gameItem.classList.add('game-item');
-            gameItem.onclick = () => {
-                window.location.href = link.href;
-            };
+            gameItem.onclick = function() { window.location.href = link.href; }
 
             const gameImage = document.createElement('img');
             gameImage.src = thumbnail;
