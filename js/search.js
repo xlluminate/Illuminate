464dd02ae8b9dae25a4e8169ead02dbeaaf0cb49
MINECRAFTGOLDFISH3 <152801036+MINECRAFTGOLDFISH3@users.noreply.github.com>
2024-06-26 14:14:00 -0500

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,5 +1,5 @@
 var allElements = [];
-var num = 760;
+var num = 899;
 for (let i = 0; i < num; i++) {
     allElements.push('<div class="card">' + document.querySelector('.card').innerHTML + '</div>');
     document.querySelector('.card').remove();
