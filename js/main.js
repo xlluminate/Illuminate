763840544f2eee8e99430a09eeb952420aaeee45
MINECRAFTGOLDFISH3 <152801036+MINECRAFTGOLDFISH3@users.noreply.github.com>
2024-09-14 15:41:40 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -59,3 +59,25 @@ var gameServer = getCookie('gamedomain');
 if (gameServer == null) {
     setCookie('gamedomain', 'projectassets.teacherease.net', 365);
 }
+
+function newad() {
+    var script = document.createElement("script");
+    script.async = true;
+    script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-331773503037412";
+    script.crossOrigin = "anonymous";
+    document.body.appendChild(script);
+    document.body.innerHTML += '<ins class="adsbygoogle" style="display:block; text-align: center;" data-ad-client="ca-pub-3317735030374126" data-ad-slot="4276812776" data-ad-format="auto" data-full-width-responsive="true"></ins>';
+    var script = document.createElement("script");
+    script.innerHTML = '(adsbygoogle = window.adsbygoogle || []).push({});';
+    document.body.appendChild(script);
+}
+
+function insertads() {
+    var script = document.createElement("script");
+    script.async = true;
+    script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-331773503037412";
+    script.crossOrigin = "anonymous";
+    document.head.appendChild(script);
+}
+
+insertads();
